74586662e822a377d6801828f9947652a1ef35b9
Otto <otto@mevo.com>
2019-10-09 19:06:38 -0400

    added in a loop

--- example.js
+++ example.js
@@ -1,3 +1,10 @@
 var otto = 'Otto'
 
 console.log(`Hello, my name is ${otto}`);
+
+var number = 10
+
+for (var i = 0; i > number; i--) {
+  console.log(`Hey, my current place in line is ${number}`)
+  number --
+}
